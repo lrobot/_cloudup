c2a52dac6249b9bebe361eaedd41af92ab0058df
root <root@localhost.localdomain>
2024-11-22 15:47:21 +0800

    m

---
+++ podman.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+#from mypriv.sh
+if test "x$BB_ASH_VERSION" != "x" ; then SCRIPT=$0; if test "x${SCRIPT}" = "x-ash" ; then SCRIPT=$1; fi; SCRIPT_DIR=$(CDPATH= cd -- "$(dirname -- "${SCRIPT}")" && pwd) ; fi
+if test "x$ZSH_VERSION" != "x" ; then SCRIPT_DIR=${0:a:h} ; fi
+if test "x$BASH_VERSION" != "x" ; then SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}" )" && pwd )" ; fi
+if test "x$SHELL" = "x/bin/ash" ; then SCRIPT_DIR=$(CDPATH= cd -- "$(dirname -- "$0")" && pwd) ; fi
+
+err_exit() {
+  echo $*
+  exit -1
+}
+
+cd $SCRIPT_DIR || err_exit
+
+
+apt update
+apt install -y podman podman-compose python3-pip git dbus-broker vim dnsmasq bzip2 golang-github-containernetworking-plugin-dnsname
+
+systemctl restart dbus-broker
+
+grep docker /etc/containers/registries.conf || {
+ echo "add docker.io for docker search"
+ echo 'unqualified-search-registries = ["docker.io"]' >> /etc/containers/registries.conf
+}
+
+# https://github.com/containers/dnsname/blob/main/README_PODMAN.md
+# https://stackoverflow.com/questions/73941967/podman-containers-cant-see-eachother-via-host-name
+#cp dnsname /usr/lib/cni/
+#mkdir -p /etc/apparmor.d/local/
+#cp usr.sbin.dnsmasq /etc/apparmor.d/local/
+#which apparmor_parser  && apparmor_parser -r /etc/apparmor.d/local/usr.sbin.dnsmasq
