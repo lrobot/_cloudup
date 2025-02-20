2c73270aba4201b5e7d5d1c7ff9e39137be3be6b
lrobot <lrobot.qq@gmail.com>
2025-02-20 12:45:32 +0800

    info

--- zerotier_relay.sh
+++ zerotier_relay.sh
@@ -3,6 +3,6 @@
 #
 podman stop zerotier_relay
 podman rm zerotier_relay > /dev/null 2>&1
-podman run -d --name zerotier_relay --init -p 445:443 -p 9995:9993/udp --rm zerotier/pylon:latest reflect
+podman run -d --name zerotier_relay --init -p 8445:443 -p 9995:9993/udp --rm zerotier/pylon:latest reflect
 
 
