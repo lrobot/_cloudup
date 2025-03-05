f96ffa72b2c6b411c7546e4c545608d3afe076bd
lrobot <lrobot.qq@gmail.com>
2025-03-05 16:27:23 +0800

    info

--- zerotier_relay.sh
+++ zerotier_relay.sh
@@ -1,8 +1,16 @@
+#!/bin/bash
 
 # https://docs.zerotier.com/relay/
 #
 podman stop zerotier_relay
 podman rm zerotier_relay > /dev/null 2>&1
 podman run -d --name zerotier_relay --init -p 8445:443 -p 9995:9993/udp --rm zerotier/pylon:latest reflect
+
+
+cat <<EOF
+todo tips:
+vi /var/lib/zerotier-one/local.conf
+{ "settings": { "tcpFallbackRelay": "1.2.3.4/8445", "forceTcpRelay": true } }
+EOF
 
 
