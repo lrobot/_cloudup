6f694cc6e051c91b8497a45042a95be5c61a92b4
lrobot <lrobot.qq@gmail.com>
2025-03-06 15:34:28 +0800

    info

---
+++ headscale/headscale.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+#from mypriv.sh  #test fromat not support by dash [[]]
+if [[ "x$BB_ASH_VERSION" != "x" ]] ; then SCRIPT=$0; if [[ "x${SCRIPT}" == "x-ash" ]]; then SCRIPT=$1; fi; SCRIPT_DIR=$(CDPATH= cd -- "$(dirname -- "${SCRIPT}")" && pwd) ; fi
+if [[ "x$ZSH_VERSION" != "x" ]] ; then SCRIPT_DIR=${0:a:h} ; fi
+if [[ "x$BASH_VERSION" != "x" ]] ; then SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}" )" && pwd )" ; fi
+if [[ "x$SHELL" == "x/bin/ash" ]] ; then SCRIPT_DIR=$(CDPATH= cd -- "$(dirname -- "$0")" && pwd) ; fi
+if [[ "x$SCRIPT_DIR" == "x" ]] ; then SCRIPT_DIR=$(CDPATH= cd -- "$(dirname -- "$0")" && pwd) ; fi #some busybox early version ont SHELL env var
+if [[ "x$SCRIPT_DIR" == "x" ]] ; then echo waring can not get SCRIPT_DIR, dont trust use it ; fi
+#[[ "x$0" == "x-ash" ]]  is source ash script case
+#
+echodo() { echo _run_cmd:"$@"; $@; }
+cd ${SCRIPT_DIR}
+
+
+podman stop headscale
+podman rm headscale
+  #--detach \
+podman run --rm \
+  --name headscale \
+  --volume $(pwd)/etc_headscale:/etc/headscale/ \
+  --publish 127.0.0.1:8080:8080 \
+  --publish 127.0.0.1:9090:9090 \
+  headscale/headscale:v0.25 \
+  serve
