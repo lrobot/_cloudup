fa109339089410ff96fd009008592b25b54488b4
lrobot <lrobot.qq@gmail.com>
2025-04-20 17:50:55 +0800

    info

--- cloudup/nfs.sh
+++ cloudup/nfs.sh
@@ -7,6 +7,11 @@ fstab_add() {
     echo "${mount_path} this host export ${mount_path}, not do fstab_add"
     return 0;
   }
+  which apk >/dev/null 2>&1 && {
+    apk add nfs-utils
+    rc-update add netmount default #this enable auto netmount in fstab
+  }
+  which apt-get >/dev/null 2>&1 && ( apt-get update; apt-get install -y nfs-common )
   mkdir -p /${mount_path}
   grep -q ${mount_path} /etc/fstab && {
     echo "${mount_path} already in /etc/fstab"
@@ -21,20 +26,20 @@ fstab_add() {
 }
 
 
-ping -c 1 qdisk.lan > /dev/null 2>&1 && {
+ping -c 1 qdisk.lan >/dev/null 2>&1 && {
   fstab_add qdisk.lan nfs.local
 }
 
 [ "x${cloudup_}" = "x" ] &&  cloudup_=$(wget -q --no-check-certificate -O- http://gitee.com/lrobot/dev_info/raw/master/cloudup_url.txt)
-ping -c 1 ${cloudup_} > /dev/null 2>&1 && {
+ping -c 1 ${cloudup_} >/dev/null 2>&1 && {
   fstab_add ${cloudup_} nfs.remote
-  which systemctl &> /dev/null && systemctl daemon-reload
+  which systemctl >/dev/null 2>&1 && systemctl daemon-reload
   [ -f /nfs.remote/shell.inc ] || {
 	   mount /nfs.remote
   }
 
 mkdir -p /etc/profile.d
-echo '[ -f /nfs.remote/shell.inc ] && source /nfs.remote/shell.inc' > /etc/profile.d/load_remote_shell.sh
+echo '{ ping -c 1 $(cat /etc/fstab|grep nfs.remote|cut -d : -f1) >/dev/null 2>&1; } && [ -f /nfs.remote/shell.inc ] && source /nfs.remote/shell.inc' > /etc/profile.d/load_remote_shell.sh
 echo 'load remote shell add ok'
 
 }
