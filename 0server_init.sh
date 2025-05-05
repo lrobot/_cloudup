1e3e7fbd77ada47e00f3fd2dc34b4e31a82ce22c
lrobot <lrobot.qq@gmail.com>
2025-05-05 17:23:59 +0800

    info

--- 0server_init.sh
+++ 0server_init.sh
@@ -5,6 +5,11 @@ if [ -f /opt/_cloudup/nfs.remote/shell.inc ] && [ ! -f /nfs.remtoe/shell.inc ] ;
   mount -o bind /opt/_cloudup/nfs.remote /nfs.remote
 fi
 
+if [ -f /opt/_cloudup/nfs.remote/shell.inc ] ; then
+cat <<EOF | tee /etc/profile.d/cloudup_nfsremote_shell.sh
+[ -f /opt/_cloudup/nfs.remote/shell.inc ] && source /opt/_cloudup/nfs.remote/shell.inc
+EOF
+fi
 
 if [ ! -e ~/file ]; then
   mkdir -p /_data/opt/_cloudup/file_rbat_tk
